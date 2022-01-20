91df359a9ea9c2bb69fd8ee120d1f55a767964ba
Babatunde Anafi <labnof@gmail.com>
2022-01-20 03:05:05 +0200

    Commit7. Improved the  Fee calculation functions

--- bin/fee_calculation_functions.dart
+++ bin/fee_calculation_functions.dart
@@ -1,10 +1,13 @@
 // Class for the Fee calculation functions.
+import 'dart:math';
+
 class FeeCalFunctions {
   // Function calculates the total delivery cost
   static double totalDeliveryFee(double cartValue, double distance,
       int itemCount, String date, String time) {
+
     double totalDeliveryFee = 0.0;
-    double maxDeliveryFee = 15.0;
+    double kMaxDeliveryFee = 15.0;
 
     // Case 0:  return 0 if the Cart Value 0 and there is no item to deliver.
     if (cartValue == 0.0 && itemCount == 0.0) {
@@ -16,37 +19,24 @@ class FeeCalFunctions {
           distanceFee(distance) +
           itemsCountSurcharge(itemCount);
 
-      //Case 1.1: totalDeliveryFee > maxDeliveryFee
-      if (totalDeliveryFee > maxDeliveryFee) {
-        return maxDeliveryFee;
-      }
-
-      //Case 1.2: fridayRush && totalDeliveryFee < maxDeliveryFee
+      //Case 1.2: fridayRush && totalDeliveryFee < kMaxDeliveryFee
       bool fridayRush = isFridayRush(date, time);
-      if (fridayRush && totalDeliveryFee < maxDeliveryFee) {
+      if (fridayRush && totalDeliveryFee < kMaxDeliveryFee) {
         totalDeliveryFee = totalDeliveryFee * 1.1;
-      
-        //Please note that isFridayRush(date, time) is expensive than Equality
-        // or relational operation. Thus the decision to include and additional
-        //rational operation instead of checking the date first.
-        //Case 1.2.1: totalDeliveryFee > maxDeliveryFee
-        if (totalDeliveryFee > maxDeliveryFee) {
-          return maxDeliveryFee;
-        }
       }
     }
-    return totalDeliveryFee;
+    return min(totalDeliveryFee, kMaxDeliveryFee);
   }
 
 // checks if it is friday rush
   static bool isFridayRush(String date, String time) {
-    num rushStartTime = 15.0;
-    num rushEndTime = 19.0;
+    num kRushStartTime = 15.0;
+    num kRushEndTime = 19.0;
 
     num givenTime =
         num.parse("${time.substring(0, 2)}.${time.substring(3, 5)}");
     bool isFriday = (DateTime.parse(date).weekday == DateTime.friday);
-    bool isRushHr = givenTime >= rushStartTime && givenTime <= rushEndTime;
+    bool isRushHr = givenTime >= kRushStartTime && givenTime <= kRushEndTime;
 
     return isFriday && isRushHr;
   }
@@ -61,31 +51,25 @@ class FeeCalFunctions {
 
   // Function calculates the distance fee
   static double distanceFee(double distance) {
-    double baseFee = 2;
-    num addedFee = 0;
-    if (distance < 1000) {
-      return baseFee;
-    } else if (distance > 1000) {
-      if ((distance - 1000) % 500 == 0) {
-        addedFee = ((distance - 1000) ~/ 500);
-        return baseFee + addedFee;
-      } else {
-        // one is added for the remainder
-        addedFee = ((distance - 1000) ~/ 500) + 1;
-        return baseFee + addedFee;
-      }
+    double kBaseFee = 2;
+    int kBaseDistance = 1000;
+    int kAdditionalDistance = 500;
+
+    if (distance < kBaseDistance) {
+      return kBaseFee;
     }
-    return baseFee + addedFee;
+    return kBaseFee + ((distance - kBaseDistance)/kAdditionalDistance).ceil();
   }
 
   // Function calculates surcharge for food items over 4
   static double itemsCountSurcharge(int itemsCount) {
     double surcharge = 0;
-    double additionalCharge = 0.50;
+    double kAdditionalCharge = 0.50;
     if (itemsCount > 4) {
-      surcharge = (itemsCount - 4) * additionalCharge;
+      surcharge = (itemsCount - 4) * kAdditionalCharge;
       return surcharge;
     }
     return surcharge;
   }
 }
+
